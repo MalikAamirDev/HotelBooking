f2f7008dbc12b0d1bd62bc5bae8ae15f3992e616
MalikAamirDev <malikaamirofficial1@gmail.com>
2021-12-16 19:05:59 +0500

    Summarize

--- src/Container/Profile.jsx
+++ src/Container/Profile.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import NavBar from "./../Components/AppBar/NavBar";
 import { Box } from "@mui/system";
-import { Paper, Typography } from "@mui/material";
+import { Button, Paper, Typography } from "@mui/material";
 import { db, getDoc, doc } from "../Config/Firebase";
 import { useDispatch } from "react-redux";
 import { getBookingData } from "../Config/Redux/action";
@@ -17,15 +17,17 @@ export default function Deshboard() {
   const userStatus = useSelector((user) => user.UserStatusReducer);
   const navigate = useNavigate();
   const [loader, setLoader] = useState(true);
-  // const [userData, setUserData] = useState([]);
   const dispatch = useDispatch();
 
+  let currentUser = getRData.userData[1];
   let booking = bookingData.bookingData[1];
-
   let uid = getRData.userData[0];
   const getData = () => {
     const docRef = doc(db, "Bookings", uid);
     dispatch(getBookingData(getDoc, docRef, setLoader));
+  };
+  const move = () => {
+    navigate("/");
   };
 
   useEffect(() => {
@@ -35,14 +37,16 @@ export default function Deshboard() {
       console.log("please login");
       navigate("/login");
     }
-  }, [navigate, userStatus]);
+  }, []);
+  console.log(getRData.userData[1].name);
+  console.log(bookingData.bookingData[1]);
 
   const userSignOut = () => {
     const auth = getAuth();
     signOut(auth)
       .then(() => {})
       .catch((error) => {
-        console.log("user Already Sign out");
+        console.log(error.message, "user Already Sign out");
       });
   };
   return (
@@ -53,40 +57,55 @@ export default function Deshboard() {
         <>
           <NavBar />
           <div>
-            <h2>Welcome, {booking.name}</h2>
+            <h2>Welcome, {currentUser.name}</h2>
             <MButton onClick={userSignOut} value="Sign Out" />
           </div>
-          <Box
-            component="div"
-            width="100%"
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <Paper
+          {booking ? (
+            <Box
+              component="div"
+              width="100%"
               sx={{
-                py: 3,
-                px: 5,
+                display: "flex",
+                justifyContent: "center",
               }}
             >
-              <Typography variant="h5" color="initial">
-                Your Booking
-              </Typography>
-              <Typography variant="subtitle2" color="initial">
-                Name: {booking.name}
-              </Typography>
-              <Typography variant="subtitle2" color="initial">
-                Contact: {booking.contact}
-              </Typography>
-              <Typography variant="subtitle2" color="initial">
-                Nights: {booking.nights}
-              </Typography>
-              <Typography variant="subtitle2" color="initial">
-                email: {booking.email}
-              </Typography>
-            </Paper>
-          </Box>
+              <Paper
+                sx={{
+                  py: 3,
+                  px: 5,
+                }}
+              >
+                <Typography variant="h5" color="initial">
+                  Your Booking
+                </Typography>
+                <Typography variant="subtitle2" color="initial">
+                  Name: {booking.name}
+                </Typography>
+                <Typography variant="subtitle2" color="initial">
+                  Contact: {booking.contact}
+                </Typography>
+                <Typography variant="subtitle2" color="initial">
+                  Nights: {booking.nights}
+                </Typography>
+                <Typography variant="subtitle2" color="initial">
+                  email: {booking.email}
+                </Typography>
+              </Paper>
+            </Box>
+          ) : (
+            <Button
+              // sx={{  }}
+              onClick={move}
+              variant="text"
+              sx={{
+                mt: 5,
+                color: "#ef3f49",
+                border: "1px solid #ef3f49",
+              }}
+            >
+              Check Hotel Rooms
+            </Button>
+          )}
         </>
       )}
     </>
